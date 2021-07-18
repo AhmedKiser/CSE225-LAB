51764e3fa1b4b34444104122fb730d53dfd1c30c
AhmedKiser <ahmed11kaiser@gmail.com>
2021-07-18 23:21:32 +0600

    header file

---
+++ Lab04/unsortedtype.h
@@ -0,0 +1,17 @@
+class unsortedtype
+{
+private:
+    /* data */
+    int length;
+public:
+    unsortedtype(/* args */);
+    ~unsortedtype();
+};
+
+unsortedtype::unsortedtype(/* args */)
+{
+}
+
+unsortedtype::~unsortedtype()
+{
+}
