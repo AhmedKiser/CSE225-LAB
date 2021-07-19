dfd91b6e1b5c71bcd1e61b2bf08b05465a12640f
AhmedKiser <ahmed11kaiser@gmail.com>
2021-07-19 22:58:49 +0600

    lab 5 included

---
+++ Lab-05 Sorted List/sortedType.h
@@ -0,0 +1,18 @@
+#ifndef SORTEDTYPE_H_INClUDED
+#define SORTEDTYPE_H_INClUDED
+template <class ItemType>
+class sortedType
+{
+private:
+    /* data */
+public:
+    sortedType();
+    void InsertItem(ItemType);
+    void DeleteItem(ItemType);
+
+    ~sortedType();
+};
+
+
+
+#endif
